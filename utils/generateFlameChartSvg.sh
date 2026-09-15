85374c847d4634912db23a52aafbcdd317daf236
Mykhailo Shevchenko <mykhailo.shev@gmail.com>
2026-09-15 17:19:52 +0200

    Further cleanup of files

--- utils/generateFlameChartSvg.sh
+++ utils/generateFlameChartSvg.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#git clone https://github.com/brendangregg/FlameGraph > /dev/null 2> /dev/null || true
+git clone https://github.com/brendangregg/FlameGraph >/dev/null 2>/dev/null || true
 
 input=$1
 output=$2
-cat $1 | ./FlameGraph/stackcollapse-gdb.pl | ./FlameGraph/flamegraph.pl > $2
+cat $1 | ./FlameGraph/stackcollapse-gdb.pl | ./FlameGraph/flamegraph.pl >$2
